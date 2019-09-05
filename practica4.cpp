5c80e69c580575c1962af4e6bdf6504fbfa3c5a7
uziel01 <uzfl27@gmail.com>
2019-09-05 09:32:33 -0500

    Es un programa a base de estructuras que permite leer datos sobre un auto para posteriormente mosytrarlos en pantalla

--- practica4.cpp
+++ practica4.cpp
@@ -1,21 +1,48 @@
-#include <bits/stdc++.h>
-using namespace std;
-int numeros[10];
+#include <iostream>
+#include<conio.h>
+using namespace std; 
 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+struct lugar{
+	char domicilio[20];
+	int calle;
+	char pais[20];
 	
-	int mayor =INT_MIN;
-	int *puntero =numeros;
-	for (;puntero<=&numeros[9];puntero++){
-		cout<<"Elemetos del arreglo: ";
-		cin>>*puntero;
-		if(*puntero>mayor) mayor=*puntero;
+}; 
+struct autoo{
+	char modelo [20];
+	int dia;
+	float precio;
+	struct lugar lu;
+};
+
+
+int main(int argc, char** argv){
+	struct autoo A1[5];
+	struct autoo *apuntador;
+	apuntador=&A1[0];
+	
+	for (int i=0;i<1;i++){
+		cout<<"Modelo: ";
+		cin.getline(A1[i].modelo,20,'\n');
+		cout<<"Dia: ";
+		cin>>A1[i].dia;
+		cout<<"Precio: ";
+		cin>>A1[i].precio;
+		cout<<"Domicilio: ";
+		fflush(stdin);
+		cin.getline(A1[i].lu.domicilio,20,'\n');
+		
 	}
-	cout<<"El numero mas grande es: "<<mayor<<endl;
-	cout<<"La direccion de memoria del arreglo"<<&numeros[10]<<endl;
-	cout<<"La direccion del puntero"<<puntero<<endl;
+	//mostrar o gardado 
+	cout<<endl<<"Modelo guardado: "<<apuntador->modelo;
+	cout<<endl<<"Año guardado: "<<A1[0].dia;
+	cout<<endl<<"Precio guardado: "<<A1[0].precio;
+	cout<<endl<<"Domicilio guardado: "<<A1[0].lu.domicilio;
 	
+	
+	
+	getch();
 	return 0;
 }
-
